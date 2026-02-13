aed02adea27bed6aa1c35f5421c6b550a1ad2bf5
minehosting <felicio.9340571@aluno.mg.gov.br>
2026-02-13 19:57:43 +0000

    fix: usar handlers específicos em api/auth/github.js e api/auth/callback.js

---
+++ api/auth/github.js
@@ -0,0 +1,10 @@
+// api/auth/github.js - Inicia login GitHub
+module.exports = (req, res) => {
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const redirectUri = encodeURIComponent(process.env.CALLBACK_URL || 'https://minehosting-seven.vercel.app/auth/github/callback');
+  const scope = encodeURIComponent('user:email repo');
+  
+  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
+  
+  res.redirect(githubAuthUrl);
+};
