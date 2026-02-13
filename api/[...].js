05cf4baf5d499187c08cbbd0cedcc253629cbd69
minehosting <felicio.9340571@aluno.mg.gov.br>
2026-02-13 19:18:34 +0000

    fix: usar rota correta no vercel.json apontando para js/server.js

--- api/[...].js
+++
@@ -1,9 +0,0 @@
-// api/[...].js - Route handler para todas as requisições
-const http = require('http');
-const serverApp = require('./server');
-
-// Criar um servidor que o Vercel pode executar
-const server = http.createServer(serverApp);
-
-// Handler para Vercel - retorna a aplicação Express
-module.exports = serverApp;
